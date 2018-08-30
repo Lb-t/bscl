896503b1044ae539260bd706451c333c6d662bac
Libi_t <libi_t@outlook.com>
2018-08-30 20:59:21 +0800

    add platform_os:

--- src/platform_task.c
+++ src/platform_task.c
@@ -2,65 +2,66 @@
 #include "platform_common.h"
 #include "platform_list_head.h"
 #include <time.h>
-
+#include "platform_os.h"
  struct platform_task_t_{
-list_head_t head;
+platform_list_head_t head;
 platform_task_routine_t routine;
 jmp_buf jumper;
    platform_scheduler_t *scheduler;
 };
  struct platform_scheduler_t_{
   jmp_buf jumper;
-  list_head_t tasks_list;
+  platform_list_head_t tasks_list;
 };
 
 void platform_scheduler_start(platform_scheduler_t *scheduler){
  
   while(1){
     platform_task_t *task;
-    list_for_each_entry(task,&scheduler->tasks_list,platform_task_t,head)
+    platform_list_for_each_entry(task,&scheduler->tasks_list,platform_task_t,head)
       {
-        int ret=setjmp(&scheduler->jumper);
+        int ret=setjmp(scheduler->jumper);
         if(ret==0){
-          longjmp(&task->jumper);
+          longjmp(task->jumper,1);
         }
       }
-    sleep(1);
+    platform_os_sleep(1);
   }
 }
 
 platform_scheduler_t* platform_scheduler_new(void){
   platform_scheduler_t *scheduler=(platform_scheduler_t *)malloc(sizeof(platform_scheduler_t));
-  list_init(&scheduler->tasks_list); 
+  platform_list_init(&scheduler->tasks_list); 
   return scheduler;
 }
 
-platform_task_t* platform_task_new(platform_scheduler_t *scheduler){
+platform_task_t* platform_task_new(platform_task_routine_t routine){
   platform_task_t *task=(platform_task_t *)malloc(sizeof(platform_task_t ));
   if(!task)
     return NULL;
-  task->scheduler=scheduler;
+  task->routine=routine;
    
   return task;
 }
 
-int platform_task_start(platform_task_t*task,void*arg){
-  list_insert_tail(&scheduler.tasks_list,&task->head) ;
-  int ret=setjmp(&task->jumper);
+int platform_task_start(platform_scheduler_t *scheduler,platform_task_t*task,void*arg){
+  platform_list_insert_tail(&scheduler->tasks_list,&task->head) ;
+  task->scheduler=scheduler;
+  int ret=setjmp(task->jumper);
   if(ret==0){return 0;}
   else{
-   ret= task->routine(arg);
-   list_head_remove(&task->head);
-   longjmp(&scheduler->jumper,ret);
+   ret= task->routine(task,arg);
+   platform_list_head_remove(&task->head);
+   longjmp(scheduler->jumper,ret);
   }
   return 0;
 }
 
 int platform_task_yeild(platform_task_t*task){
-  int ret=setjmp(&task->jumper); 
+  int ret=setjmp(task->jumper); 
   if(ret==0)
     {
-      longjmp(&task->scheduler->jumper,0);
+      longjmp(task->scheduler->jumper,0);
 
     }
   return 0; 

62f7b0a79a1e21bfb21fb835de210c8b59cd400e
liubin-011 <liubin-011@tyiee.com>
2019-08-09 15:56:07 +0800

    sterm for linux

--- tools/sterm/sterm.c
+++ tools/sterm/sterm.c
@@ -1,31 +1,60 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
+#if defined(_WIN32)
 #include <windows.h>
+#include <conio.h>
+#elif defined(__linux__)
+#include "curses.h"
+#endif
+
 #include "bscl_serialport.h"
 #include "bscl_os.h"
-#include <conio.h>
+
+#if defined(_WIN32)
+#define DEV_NAME "COM5"
+#elif defined(__linux__)
+#define DEV_NAME "/dev/ttyS5"
+#endif
 
 void init(void) {
+#if defined(_WIN32)
   HANDLE hStdin = GetStdHandle(STD_INPUT_HANDLE);
   DWORD mode = 0;
   GetConsoleMode(hStdin, &mode);
   SetConsoleMode(hStdin, mode & (~(ENABLE_ECHO_INPUT | ENABLE_LINE_INPUT)));
+#elif defined(__linux__)
+  WINDOW *w;
+  w = initscr();
+  noecho();
+  setbuf(stdout, NULL);
+#endif
+}
+
+void deinit(void) {
+#if defined(_WIN32)
+#elif defined(__linux__)
+  endwin();
+#endif
 }
 
 void *recv_task(void *arg) {
   int fd = *(int *)arg;
   char c;
   while (1) {
+
     bscl_serialport_read(fd, &c, 1);
     putchar(c);
+    // printf("\r\nrecv %x\n\r",c);
+
+    fflush(stdin);
   }
   return NULL;
 }
 
 int main(void) {
   init();
-  int fd = bscl_serialport_open("COM3");
+  int fd = bscl_serialport_open(DEV_NAME);
   if (fd < 0) {
     printf("fail\n");
     getchar();
@@ -46,12 +75,9 @@ int main(void) {
   bscl_os_thread_create(&tid, NULL, recv_task, &fd);
   while (1) {
     int c = getch();
-    if (c < 0) {
-      continue;
-    } else {
-      char buf = c;
-      bscl_serialport_write(fd, &buf, 1);
-    }
+    char buf = c;
+    // printf("write %x\n\r",buf);
+    bscl_serialport_write(fd, &buf, 1);
   }
   printf("exit\n");
   return 0;

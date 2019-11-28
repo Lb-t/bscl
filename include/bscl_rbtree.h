731a0a1c258e1f456618f55f2cbac06f1046f2b3
liubin-011 <liubin-011@tyiee.com>
2019-11-28 17:36:44 +0800

    add rbtree

--- include/bscl_rbtree.h
+++ include/bscl_rbtree.h
@@ -2,8 +2,16 @@
 
 typedef enum { BSCL_RBTREE_RED, BSCL_RBTREE_BLACK } bscl_rbtree_color_t;
 
-typedef struct bscl_rbtree_t_ {
-  struct bscl_rbtree_t_ *p;
-  struct bscl_rbtree_t_ *l;
-  struct bscl_rbtree_t_ *r;
+typedef struct bscl_rbtree_node_t_ {
+  struct bscl_rbtree_node_t_ *p;
+  struct bscl_rbtree_node_t_ *l;
+  struct bscl_rbtree_node_t_ *r;
+  bscl_rbtree_color_t color;
+} bscl_rbtree_node_t;
+
+typedef int (*bscl_rbtree_comp_t)(bscl_rbtree_node_t *x, bscl_rbtree_node_t *y);
+
+typedef struct {
+  bscl_rbtree_node_t *root;
+  bscl_rbtree_comp_t comp;
 } bscl_rbtree_t;
